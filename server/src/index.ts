37fbc21b630849d873e0a0398705ea1cc705cc00
Kedar Bargule <kedarbargule@gmail.com>
2025-09-22 13:05:46 +0530

    serversetup

---
+++ server/src/index.ts
@@ -0,0 +1,35 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import helmet from "helmet";
+import bodyParser from "body-parser";
+import morgan from "morgan";
+
+
+
+/* configs */
+
+dotenv.config();
+const app = express();
+
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({policy:"cross-origin"}));
+app.use(morgan("common"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(cors());
+
+
+
+/*routes*/
+app.get('/', (req,res)=>{
+    res.send("this is home route");
+});
+
+
+/*actual server*/
+const port = process.env.PORT || 3002;
+app.listen(port, ()=>{
+    console.log("server is running");
+})
